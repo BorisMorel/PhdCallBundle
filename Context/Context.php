13323b7fac1f8f9d556ba0d2805413ebf9c1625c
Boris Morel <Boris.Morel@imag.fr>
2012-08-31 10:10:37 +0200

    service to manage application context

---
+++ Context/Context.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace IMAG\PhdCallBundle\Context;
+
+class Context
+{
+    private
+        $config = array()
+        ;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+}
